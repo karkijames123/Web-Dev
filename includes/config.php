902310bceab2e59c46d6cdcd608fa4a195a124dc
NabinKarki <145247760+karkijames123@users.noreply.github.com>
2026-03-28 13:55:37 +0100

    Update SITE_URL in configuration file

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 // Configuration file
 define('SITE_NAME', 'Global Tech University');
-define('SITE_URL', 'http://localhost/Web-Dev-Crew');
+define('SITE_URL', 'http://localhost/Web-Dev');
 define('ADMIN_EMAIL', 'admin@university.com');
 define('UPLOAD_DIR', __DIR__ . '/../uploads/');
 define('MAX_FILE_SIZE', 2097152);
